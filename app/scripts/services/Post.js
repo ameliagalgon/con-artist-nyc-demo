70b37686107860468290532cd3bd1802bc4cbbe9
ameliagalgon <amg617@lehigh.edu>
2017-09-07 13:44:52 -0400

    expanding more robust. only one post can be expanded at a time

--- app/scripts/services/Post.js
+++ app/scripts/services/Post.js
@@ -10,17 +10,27 @@
         Post.expanded = false;
 
         Post.togglePost = function(post){
-            console.log(post);
-
             var element = $(document.getElementById(post.$id));
 
             if(element.hasClass("expanded")){
                 element.find('.preview').css("display","block");
                 element.find('.full-content').css("display","none");
             } else{
+                var expandedElements = $('.expanded');
+                //console.log(expandedElements);
+                if(expandedElements.length > 0){
+                    $.each(expandedElements, function(){
+                        $(this).removeClass("expanded");
+                        $(this).find('.preview').css("display","block");
+                        $(this).find('.full-content').css("display","none");
+                    });
+                }
+
                 element.find('.preview').css("display","none");
                 element.find('.full-content').css("display","block");
             }
+            //find other divs with the expanded class
+
             element.toggleClass("expanded");
         };
 
